efec814f282fd4c23868b3afd565a5fca53c7579
Michał Śledź <michalsledz34@gmail.com>
2022-10-01 12:46:45 +0200

    Initial commit

---
+++ lib/ex_turn.ex
@@ -0,0 +1,18 @@
+defmodule ExTurn do
+  @moduledoc """
+  Documentation for `ExTurn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTurn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_turn_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTurnTest do
+  use ExUnit.Case
+  doctest ExTurn
+
+  test "greets the world" do
+    assert ExTurn.hello() == :world
+  end
+end
