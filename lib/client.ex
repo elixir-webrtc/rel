a2475620c407ab6f669ffd8335545fdcf3e0dc83
Michał Śledź <michalsledz34@gmail.com>
2022-12-03 13:48:26 +0100

    Respond with nonce and realm

--- lib/client.ex
+++ lib/client.ex
@@ -4,14 +4,15 @@ defmodule ExTURN.Client do
   alias ExStun.Message
   alias ExStun.Message.{Attribute, Type}
 
-  def serve(client) do
+  def serve(socket) do
     receive do
-      {:tcp_closed, ^client} ->
-        Logger.info("TCP connection closed. Closing client #{inspect(client)}")
+      {:tcp_closed, ^socket} ->
+        Logger.info("TCP connection closed. Closing client #{inspect(socket)}")
 
       {:tcp, _port, msg} ->
         with {:ok, msg} <- ExStun.Message.decode(msg) do
-          handle_message(msg)
+          response = handle_message(msg)
+          :gen_tcp.send(socket, response)
         else
           {:error, reason} ->
             Logger.warn("""
@@ -19,7 +20,7 @@ defmodule ExTURN.Client do
             """)
         end
 
-        serve(client)
+        serve(socket)
     end
   end
 
@@ -42,9 +43,25 @@ defmodule ExTURN.Client do
   end
 
   defp authenticate(%Message{} = msg) do
+    # IO.inspect(msg)
     case Attribute.MessageIntegrity.get_from_message(msg) do
-      nil -> Logger.info("No message integrity attribute. Seems like a new allocation.")
-      attr -> IO.inspect(attr)
+      nil ->
+        Logger.info("No message integrity attribute. Seems like a new allocation.")
+        type = %Type{class: :error_response, method: :allocate}
+        response = Message.new(msg.transaction_id, type)
+        nonce = %Attribute.Nonce{value: "testnonce"}
+        realm = %Attribute.Realm{value: "testrealm"}
+        error_code = %Attribute.ErrorCode{code: 401}
+        response = Attribute.Nonce.add_to_message(nonce, response)
+        response = Attribute.ErrorCode.add_to_message(error_code, response)
+
+        Attribute.Realm.add_to_message(realm, response)
+        |> Message.encode()
+
+      attr ->
+        IO.inspect(msg)
+        Logger.info("Got message integrity, #{inspect(attr)}")
+        ""
     end
   end
 end
